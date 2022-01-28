2b8c4100459d6b7bb7e56120fc53f1ef3a190d6c
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2022-01-28 15:20:21 -0300

    Criação da Classe Person

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Person.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digionebank
+
+class Person {
+    var name: String = "Rafael"
+    var cpf: String = "123.123.123-12"
+}
+
+fun main() {
+    val rafael = Person()
+
+    println(rafael.name)
+    println(rafael.cpf)
+}
